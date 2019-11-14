ecf04b6d2fb5e113d24c248c9fccc8a2f665a549
amenprog <amenprog@gmail.com>
2019-11-14 04:18:50 +0200

    clean up

--- async-for.js
+++ async-for.js
@@ -5,23 +5,15 @@
      * inputArray
      * onEachAction: (item, index, onEachResolve())
      * onEachResolve: (item, index, data, isDone)
-     * onError
      */
-    function seqProcess(inputArray, onEachAction, onEachResolve) {//, onAll, initialValue, onEachSuccess, onError) {
+    function seqProcess(inputArray, onEachAction, onEachResolve) {
         var arr = inputArray || [];
 
-        // var accumulator = initialValue;
-        // if(arr.length === 0) {
-        //     onAll(accumulator);
-        //     return;
-        // }
-
-        processOne(inputArray, 0, onEachAction, onEachResolve);
+        processOne(arr, 0, onEachAction, onEachResolve);
     }
 
     function processOne(inputArray, index, onEachAction, onEachResolve) {
         if(index >= 0 && index < inputArray.length) {
-            //console.log(`exec nb: ${index}`);
             onEachAction(inputArray[index], index, function (item, nextIndex, data) {
                 onEachResolve(item, nextIndex, data, nextIndex === inputArray.length - 1);
                 if(index < inputArray.length - 1) {
